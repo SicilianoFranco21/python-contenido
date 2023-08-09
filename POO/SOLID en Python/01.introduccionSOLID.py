a39266bf3ad9ce548f608cab76d29cdd4f3f5542
sicilianofranco21 <sicilianofranco21@gmail.com>
2023-08-09 14:25:17 -0300

    Re-estructurar archivo 01.introduccionSOLID.py

---
+++ POO/SOLID en Python/01.introduccionSOLID.py
@@ -0,0 +1,20 @@
+"""
+>>> S.O.L.I.D: Representa cinco principios de diseño de software que buscan crear sistemas
+            mas grandes, mantenibles y flexibles.
+            Estos principios se centran en ayudar a escribir codigo limpio y estructurado que
+            sea mas facil de entender y modificar con el tiempo, por lo tanto, reduce la probablidad de
+            errores a medida que el proyecto evoluciona
+
+    >>> RECOMENDACION: Aplicar los principios SOLID puede mejorar la calidad del codigo
+            y la arquitectura del software
+
+    >>> CUANDO USARLO? Usarlo para la POO cuando se desee hacer un proyecto grande el cual
+            se desee que sea escalable, mantenible y mas organizado
+
+    >>> PRINCIPIOS SOLID:
+        [1] - SRP (Single Responsibility Principle)
+        [2] - OCP (Open/Closed Principle)
+        [3] - LSP (Liskov Substitution Principle)
+        [4] - ISP (Interface Segregation Principle)
+        [5] - DIP (Dependency Inversion Principle)
+"""
