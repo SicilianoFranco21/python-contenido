ae7abf18dd3e575b0b2c3e5b93155456cd12da54
sicilianofranco21 <sicilianofranco21@gmail.com>
2023-08-10 00:29:47 -0300

    Agregar 04.LSP.py + contenido teorico del mismo

---
+++ POO/SOLID en Python/04.LSP.py
@@ -0,0 +1,17 @@
+"""
+>>> LSP: Se enfoca en como las sub-clases deben relacionarse con las superclases en un sistema de herencia.
+        Establece que una subclase (ya sea actualizada o mejorada con respecto a la clase base) debe poder ser usada
+        en lugar de su clase base sin alterar el comportamiento del programa. Es decir, la sub-clase
+        SUSTITUYE a la clase base sin alterar el programa
+        - ENFOQUE --> Relacion entre clases y subclases, sustitucion de clase por subclase en los lugares donde la primera existia
+
+    >>> EJEMPLO: Construimos una clase Jugador que puede moverse, luego construimos una clase hija JugadorMejorado
+                que se mueve mas rapido, segun el LSP, la sub-clase JugadorMejorado deberia SUSTITUIR efectivamente a la clase
+                Jugador en todos los lugares donde esta ultima fue implementada
+
+    >>> VENTAJAS
+        [1] - EVITA HERENCIA INCORRECTA: Previene situaciones en la que una subclase no puede reemplazar correctamente a su clase base
+        [2] - PROMUEVE EL REUSO DE CODIGO: LSP fomenta el reuso del codigo y la creacion de jerarquias de clases mas cohesivas (Esto significa MUY relacionadas entre si para que las partes vinculadas trabajen mejor)
+        [3] - FACILITA LA EXTENSIBILIDAD: Disenio ideal para agregar nuevas clases derivadas (subclases) sin afectar el comportamiento existente
+        [4] - MEJORA LA CLARIDAD DEL DISENIO: Evita el comportamiento inesperados y las relaciones entre clases y subclases se mantienen claras y coherentes
+"""
