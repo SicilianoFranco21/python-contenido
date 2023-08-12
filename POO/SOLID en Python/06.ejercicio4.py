8cd877d90849cfcf17898d38b6bed8505e4f38ed
sicilianofranco21 <sicilianofranco21@gmail.com>
2023-08-12 17:01:31 -0300

    Agregar enunciado y de 06.ejercicio4.py

---
+++ POO/SOLID en Python/06.ejercicio4.py
@@ -0,0 +1,11 @@
+"""
+EJERCICIO con el proposito de aplicar la mayor cantidad de conceptos posibles de POO
+(Sin descuidar los principios S.O.L.I.D)
+
+>>> CREAR UN JUEGO DE RPG (Jugable desde consola unicamente)
+
+    -> Descripcion:
+    -> Requerimientos: 
+    -> Criterios de aceptacion:  
+
+"""
