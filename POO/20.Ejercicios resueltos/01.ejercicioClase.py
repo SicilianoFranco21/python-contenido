ee184e6be368d80f1898db5d380b81aa22e2ad36
SicilianoFranco21 <sicilianofranco21@gmail.com>
2023-08-15 23:55:13 -0300

    Resolver 01.ejercicioClase.py

--- POO/20.Ejercicios resueltos/01.ejercicioClase.py
+++ POO/20.Ejercicios resueltos/01.ejercicioClase.py
@@ -2,3 +2,23 @@
 Crea una clase llamada Persona que tenga atributos como nombre y edad. 
 Luego, crea objetos de esta clase y muestra sus atributos.
 """
+
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre: str = nombre
+        self.edad: int = edad
+
+
+persona1: Persona = Persona("Agustina", 24)
+persona2: Persona = Persona("Santino", 18)
+
+
+def presentarse(*args: Persona) -> None:
+    contador_personas: int = 0
+    for persona in args:
+        contador_personas += 1
+        print(f"[{contador_personas}] --> Hola me llamo {persona.nombre} y mi tengo {persona.edad} anios")
+
+
+presentarse(persona1, persona2)
