bae872ae6c66a695bf9c6e915df6b24f0e758b0f
SicilianoFranco21 <sicilianofranco21@gmail.com>
2023-08-17 01:30:00 -0300

    Actualizar ejercicio 06 en '20.Ejercicios resueltos'

--- POO/20.Ejercicios resueltos/06.ejercicioPolimorfismo.py
+++ POO/20.Ejercicios resueltos/06.ejercicioPolimorfismo.py
@@ -1,6 +1,8 @@
 """
-Crea una clase base Animal con un método hacer_sonido().
-Luego, crea subclases como Perro, Gato y Vaca, que
-sobrescriban este método y hagan sonidos diferentes.
+Crea una jerarquía de clases que representen diferentes
+instrumentos musicales, como guitarras y pianos. Cada instrumento 
+puede tener un método tocar() que imprima un mensaje. Luego,
+crea una función que tome una lista de instrumentos y haga que
+cada uno sea tocado.
 """
 # RECOMENDACION: Expresar libertad creativa en el ejercicio. No descuidar el enfoque del mismo y que funcione correctamente
