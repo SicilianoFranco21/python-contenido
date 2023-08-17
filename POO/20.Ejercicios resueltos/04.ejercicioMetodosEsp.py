68d998ff61641bbda51a3c69a187ae12ea5b8ff1
SicilianoFranco21 <sicilianofranco21@gmail.com>
2023-08-16 22:54:16 -0300

    Resolver ejercicio 04 de '20.Ejercicios resueltos'

--- POO/20.Ejercicios resueltos/04.ejercicioMetodosEsp.py
+++ POO/20.Ejercicios resueltos/04.ejercicioMetodosEsp.py
@@ -3,3 +3,19 @@ Crea una clase Punto que represente un punto en un plano 2D.
 Implementa el método __str__ para imprimir el punto de manera legible.
 """
 # RECOMENDACION: Expresar libertad creativa en el ejercicio. No descuidar el enfoque del mismo y que funcione correctamente
+
+
+class Punto:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+    
+    def __str__(self):
+        return f"({self.x}; {self.y})"
+
+
+punto1: Punto = Punto(3, 4.5)
+punto2: Punto = Punto(-2, 3)
+
+
+print(punto1, punto2)
