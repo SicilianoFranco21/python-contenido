45f580243d0d37e59596ea56d2de4c0394638d36
SicilianoFranco21 <sicilianofranco21@gmail.com>
2023-08-19 18:54:15 -0300

    Completar ejercicio 06 de '20.Ejercicios resueltos'

--- POO/20.Ejercicios resueltos/06.ejercicioPolimorfismo.py
+++ POO/20.Ejercicios resueltos/06.ejercicioPolimorfismo.py
@@ -6,3 +6,39 @@ crea una función que tome una lista de instrumentos y haga que
 cada uno sea tocado.
 """
 # RECOMENDACION: Expresar libertad creativa en el ejercicio. No descuidar el enfoque del mismo y que funcione correctamente
+
+
+
+# Crear clase generica instrumento
+class Instrumento:
+    def tocar(self):
+        pass
+
+
+# Crear sub-clases que heredan de instrumento
+class Guitarra(Instrumento):
+    def tocar(self):
+        print("La guitarra esta sonando")
+
+
+class Piano(Instrumento):
+    def tocar(self):
+        print("El piano esta sonando")
+
+
+class Violin(Instrumento):
+    def tocar(self):
+        print("El violin esta sonando")
+        
+
+instrumentos: list = [Guitarra(), Piano(), Violin(), Guitarra()]
+
+
+# Definir procedimiento para tocar todos los instrumentos
+def tocar_instrumento(instrumentos: list) -> None:
+    for instrumento in instrumentos:
+        instrumento.tocar()
+    
+
+# Ejecutar procedimiento    
+tocar_instrumento(instrumentos)
