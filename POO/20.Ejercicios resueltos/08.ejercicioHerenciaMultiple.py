9a0381814377fb2116328d0a0dcce67706d54894
SicilianoFranco21 <sicilianofranco21@gmail.com>
2023-08-21 03:24:53 -0300

    Resolver ejercicio 08 y 09 de '20.Ejercicios resueltos'

--- POO/20.Ejercicios resueltos/08.ejercicioHerenciaMultiple.py
+++ POO/20.Ejercicios resueltos/08.ejercicioHerenciaMultiple.py
@@ -1,6 +1,45 @@
 """
-Crea una clase Persona con atributos como nombre y edad.
-Luego, crea una clase Trabajador que herede de Persona y 
-tenga atributos adicionales como salario y puesto de trabajo.
+Crea una jerarquía de clases que modele diferentes roles en una institución educativa. Deberás implementar las siguientes clases:
+> Persona: Una clase base que tenga atributos comunes como nombre y edad.
+> Estudiante: Una clase que tenga atributos como institucion y curso.
+> Trabajador: Una clase que tenga atributos como salario y puesto.
+> TrabajadorEstudiante: Una clase que herede tanto de Trabajador como de Estudiante. Debe tener todos los atributos de ambas clases base.
+Crea instancias de la clase TrabajadorEstudiante y muestra por pantalla los atributos de cada instancia.
 """
 # RECOMENDACION: Expresar libertad creativa en el ejercicio. No descuidar el enfoque del mismo y que funcione correctamente
+
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+
+class Estudiante:
+    def __init__(self, institucion, curso):
+        self.institucion = institucion
+        self.curso = curso
+
+
+class Trabajador:
+    def __init__(self, salario, puesto):
+        self.salario = salario
+        self.puesto = puesto
+
+
+class TrabajadorEstudiante(Persona, Estudiante, Trabajador):
+    def __init__(self, nombre, edad, institucion, curso, salario, puesto):
+        Persona.__init__(self, nombre, edad)
+        Estudiante.__init__(self, institucion, curso)
+        Trabajador.__init__(self, salario, puesto)
+
+
+trabajador_estudiante: TrabajadorEstudiante = TrabajadorEstudiante("Franco", 25, "UBA", "Quimica II A", 500.25, "QA")
+
+
+print("Nombre ->", trabajador_estudiante.nombre) 
+print("Edad ->", trabajador_estudiante.edad) 
+print("Institucion ->", trabajador_estudiante.institucion) 
+print("Curso ->", trabajador_estudiante.curso)   
+print("Salario ->", trabajador_estudiante.salario)
+print("Puesto ->", trabajador_estudiante.puesto)
--- POO/20.Ejercicios resueltos/09.ejercicioAbstraccion.py
+++ POO/20.Ejercicios resueltos/09.ejercicioAbstraccion.py
@@ -4,3 +4,26 @@ abstracto calcular_area(). Luego, crea subclases como
 Circulo y Rectangulo que implementen este método.
 """
 # RECOMENDACION: Expresar libertad creativa en el ejercicio. No descuidar el enfoque del mismo y que funcione correctamente
+
+
+from abc import ABC, abstractclassmethod, abstractmethod
+
+
+class FiguraGeometrica(ABC):
+    @abstractclassmethod
+    def calcular_area():
+        pass
+    
+
+class Circulo(FiguraGeometrica):
+    def calcular_area(r: float, pi: float = 3.14):
+        return pi*(r**2)
+    
+
+class Rectangulo(FiguraGeometrica):
+    def calcular_area(base: float, altura: float):
+        return base*altura
+
+
+print("Area circulo:", Circulo.calcular_area(4))
+print("Area rectangulo:", Rectangulo.calcular_area(3, 4))
